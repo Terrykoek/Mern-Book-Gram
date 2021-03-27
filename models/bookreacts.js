5eae652241bd47a0edd3566808fab29181b11511
root <root@DESKTOP-UA20VBG.localdomain>
2021-03-27 23:33:24 +0800

    set up backend data in postman and mongoatlas

--- models/bookreacts.js
+++ models/bookreacts.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const bookreactSchema = new mongoose.Schema({
     title: String,
+    sypnopsis: String,
     url: String,
+    likeCount: String
 });
 
 const Bookreacts = mongoose.model('Bookreact', bookreactSchema)
