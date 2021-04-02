ca98cbacb70f9bfb59a3575154777414eaab6433
Dorothy Du <dorothydu@MacBook-Pro-3.local>
2021-04-02 23:18:52 +0800

    Added Compnents -login page withgoogle login

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,23 @@
 import React , { Component } from "react";
 import GoogleLogin from "react-google-login";
-// import axios from "axios";
+// import GoogleLogin from "react-google-login";
+import Login from './components/login';
 
-// function App() {
-//   const responseSuccessGoogle = (response) => {
-//     console.log(response);
-    
-//   };
-//   const responseErrorGoogle = (response) => {
-//     console.log(response);
-//   };
-//   return (
-//     <div className="App">
-//       <h1>login with google</h1>
-//       <GoogleLogin
-//         clientId="1044015139792-jjl22brbh17oo2a7jkgg720tcqqc6cgf.apps.googleusercontent.com"
-//         buttonText="Login with Google"
-//         onSuccess={responseSuccessGoogle}
-//         onFailure={responseErrorGoogle}
-//         cookiePolicy={"single_host_origin"}
-//       />
-      
-//     </div>
-//   );
-// }
+
 
 export class App extends Component {
 
-    responseGoogle = (response) => {
-    console.log(response);
-    console.log(response.profileObj);
+    // responseGoogle = (response) => {
+    //   console.log(response.accessToken);
+    // // console.log(response);
+    // console.log(response.profileObj);
 
-    }
+    // }
   render() {
     return (
-      <div>
-        <GoogleLogin
-        clientId="697400510228-eacr051p1dkqcdp5j8clv60bi365kih3.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
-        cookiePolicy={'single_host_origin'}
-
-        />
+      <div className="App">
+        <Login/>
+        
       </div>
     )
   }
@@ -51,3 +26,6 @@ export class App extends Component {
 
 
 export default App;
+
+
+
